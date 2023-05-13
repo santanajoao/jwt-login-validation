316b209e0fa967a765e58d4e0d8df4defcdfe7dd
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-13 13:59:44 -0300

    cria model de usuário

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User;
+};
