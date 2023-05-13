0898effa8622b794216ba353d4d46ef77fc13759
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-12 22:47:52 -0300

    cria página principal

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,28 @@
+import { AiOutlineLike } from 'react-icons/ai';
+import styles from './page.module.css';
+
 export const metadata = {
   title: 'Página inicial',
 }
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home Page</h1>
-      <p>Essa página requere autenticação. Se o usuário não estiver logado deve ser redirecionado para a página de sign-in. Ao logar deve ser redirecionado para cá.</p>
-    </main>
+    <> 
+      <header className={styles.header}>
+        <div className={styles.user_container}>
+          <span className={styles.user_message}>
+            Olá, <strong>nome do usuário</strong>!
+          </span>
+          <button type="button" className={styles.logout_button}>Sair</button>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.main_message}>
+          <AiOutlineLike className={styles.like} />
+          <h1>Logado e Autenticado</h1>
+          <p>Se você está aqui, você está logado!</p>
+        </div>
+      </main>
+    </>
   )
 }
